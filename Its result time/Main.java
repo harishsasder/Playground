3df14e67e3a5dc21eebf327118f857f47015ccd9
harishsasder <harishsasder@gmail.com>
2020-05-12 12:57:07 +0530

    Test code pushed to repository.

---
+++ Its result time/Main.java
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  int Id;
+  int Totalmarks;
+  std::cin>>Id;
+  std::cout<<"Id : "<<Id;
+  std::cin>>Totalmarks;
+  std::cout<<"\nTotalmarks : "<<Totalmarks;
+}
+  
